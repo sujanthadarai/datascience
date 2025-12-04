c37300dea44a516ddf0feb972ed55925b94b1e15
= <sujanthadarai710@gmail.com>
2025-12-04 14:07:54 +0545

    first change

---
+++ file2.py
@@ -0,0 +1,12 @@
+print("this is file code ")
+
+'''
+Git & Github
+Git -->version control system
+# it track out all histry or manage our file 
+# iit help to create file ,delete ,save ,resave in  file 
+
+Github: it is website that store project on remote by using git 
+local folder ---> stage area ---->local repo -->repo
+
+'''
